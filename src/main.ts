111acb0a41b056b3b0537329a6b430cd84d9624c
Eduardo <eduzamorano@gmail.com>
2025-01-09 01:45:31 -0300

    cabios

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
-import { HttpClientExportsModule } from './app/http-client.module';
-import { HttpClientModule } from '@angular/common/http';
+
 
 bootstrapApplication(AppComponent, {
-  ...appConfig,
-  imports: [HttpClientExportsModule, HttpClientModule
-  ]
-} as any)
-  .catch((err) => console.error(err));
+  providers: [provideHttpClient()]
+}).catch(err => console.error(err));
